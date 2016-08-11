d2bc2ae693c8f3e6f716d5fc1bdfba0f977a700a
Dima Lupyak <dima.lupyak@gmail.com>
2016-08-11 14:09:26 +0300

    changed target platform to 4.5

--- StudentTestsDataBaseEntities/StudentTestDataModel.Designer.cs
+++ StudentTestsDataBaseEntities/StudentTestDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Programming\VisualStudioProjects\Delphi\StudentTests\StudentTests\StudentTestsDataBaseEntities\StudentTestDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Programming\Study Projects\StudentTests\StudentTestsDataBaseEntities\StudentTestDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
